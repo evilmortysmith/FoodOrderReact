6b645a6ee61463fb1a4fa07b621be166aacb379d
GeekWolf007 <geeksingal@gmail.com>
2023-08-04 23:57:30 +0530

    header

--- src/Components/Layout/Header.jsx
+++ src/Components/Layout/Header.jsx
@@ -1,11 +1,20 @@
 import React, { useState } from 'react';
-import styles from "../Layout/Header.module.css"
+import styles from "../Layout/Header.module.css";
+import mealsImg from "../../assets/meals.jpg";
+import HeaderCartButton from './HeaderCartButton';
 
-const Header = () => {
+
+const Header = (props) => {
     return (
-        <>
-            
-        </>
+        <React.Fragment>
+            <header className={styles["header"]}>
+                <h1>ReactMeals</h1>
+                <HeaderCartButton />
+            </header>
+            <div className={styles["main-image"]}>
+                <img src={mealsImg} alt="A table full of delilcious food!" />
+            </div>
+        </React.Fragment>
     );
 }
 
