f796302a8143e5985eb63c084ba07f0e6719efb7
GeekWolf007 <geeksingal@gmail.com>
2023-08-04 23:21:51 +0530

    commit

---
+++ src/Components/Layout/Header.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import styles from "../Layout/Header.module.css"
+
+const Header = () => {
+    return (
+        <>
+            
+        </>
+    );
+}
+
+export default Header;
