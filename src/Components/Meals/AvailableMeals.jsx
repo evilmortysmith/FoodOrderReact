35a488783e359ca097b62b55acf9cae6172af182
GeekWolf007 <geeksingal@gmail.com>
2023-08-05 00:34:35 +0530

    menu items

--- src/Components/Meals/AvailableMeals.jsx
+++ src/Components/Meals/AvailableMeals.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import styles from "./AvailableMeals.module.css"
+import Card from '../UI/Card';
+import MealItem from './MealItem/MealItem';
 
 const DUMMY_MEALS = [
     {
@@ -29,12 +31,14 @@ const DUMMY_MEALS = [
 ];
 
 const AvailableMeals = () => {
-    const mealsList = DUMMY_MEALS.map(meal => <li key={meal.id}>{meal.name}</li>);
+    const mealsList = DUMMY_MEALS.map(meal => <MealItem key={meal.id} name={meal.name} price = {meal.price} description={meal.description} />);
     return (  
         <section className={styles["meals"]}>
-            <ul>
-                {mealsList}
-            </ul>
+            <Card>
+                <ul>
+                    {mealsList}
+                </ul>
+            </Card>
         </section>
     );
 }
