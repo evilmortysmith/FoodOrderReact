2b0e82b74c3d5c1ca44e64047e99130078ee1dab
GeekWolf007 <geeksingal@gmail.com>
2023-08-07 01:21:10 +0530

    useReducer and context

--- src/Components/Layout/HeaderCartButton.jsx
+++ src/Components/Layout/HeaderCartButton.jsx
@@ -1,16 +1,25 @@
 import React from 'react';
+import { useContext } from 'react';
+import CartContext from '../../store/cart-context';
 import styles from "../Layout/HeaderCartButton.module.css";
 import CartIcon from "../Layout/CartIcon";
 
-const HeaderCartButton = () => {
+const HeaderCartButton = (props) => {
+
+    const cartCtx = useContext(CartContext);
+
+    const numberOfCartItems = cartCtx.items.reduce((curNumber , item) => {
+        return curNumber + item.amount;
+    },0);
+
     return (  
-        <button className={styles["button"]}>
+        <button className={styles["button"]} onClick={props.onClick}>
             <span className={styles['icon']}>
                 <CartIcon />
             </span>
             <span>Your Cart</span>
             <span className={styles['badge']}>
-                3
+                {numberOfCartItems}
             </span>
         </button>
     );
--- src/Components/UI/Modal.jsx
+++ src/Components/UI/Modal.jsx
@@ -3,7 +3,7 @@ import styles from "./Modal.module.css"
 import ReactDOM from 'react-dom';
 
 const BackDrop = (props) => {
-    return <div className={styles.backdrop}></div>
+    return <div className={styles.backdrop} onClick={props.onClose}></div>
 };
 const ModalOverlay = (props) => {
     return <div className={styles.modal}>
@@ -16,7 +16,7 @@ const portalElem = document.getElementById('overlays');
 const Modal = (props) => {
     return (  
         <React.Fragment>
-            {ReactDOM.createPortal(<BackDrop /> , portalElem)}
+            {ReactDOM.createPortal(<BackDrop onClose={props.onClose} /> , portalElem)}
             {ReactDOM.createPortal(<ModalOverlay>{props.children}</ModalOverlay> , portalElem)}
         </React.Fragment>
     );
